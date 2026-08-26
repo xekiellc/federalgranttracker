41ee168999168816f87d9e167007567ffc5ccea3
xekiellc <zach@xekie.com>
2026-08-25 21:35:25 -0400

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+
+export default defineConfig({
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+  site: 'https://federalgranttracker.com'
+});
